a9c35d094975f7d202a892c3fce3d45348b3a771
Guillom <147560209+Gui-llom@users.noreply.github.com>
2025-12-02 09:25:41 +0100

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int a = 2;     
+    int b = 3;     
     int resultat = 1;
 
-    printf("Entrez a : ");
-    scanf("%d", &a);
-
-    printf("Entrez b : ");
-    scanf("%d", &b);
-
-    if (b < 0) {
-        printf("Erreur : b doit être positif.\n");
-        return 1;
-    }
 
     for (int i = 0; i < b; i++) {
         resultat *= a;
     }
 
-    printf("%d^%d = %d\n", a, b, resultat);
+    printf("%d élevé à la puissance %d = %d\n", a, b, resultat);
 
     return 0;
 }
