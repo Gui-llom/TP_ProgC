b232b6e8746b34c641f7032ea3f9844fbaf68891
Guillom <147560209+Gui-llom@users.noreply.github.com>
2025-12-02 09:25:02 +0100

    Update fibonacci.c

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    int somme = 0;
+    int n = 7;  
+    int a = 0, b = 1, temp;
 
-    printf("Entrez un entier n : ");
-    scanf("%d", &n);
+    printf("%d, %d", a, b);
 
-    if (n <= 0) {
-        printf("Erreur : n doit être positif.\n");
-        return 1;
+    for(int i = 2; i <= n; i++) {
+        temp = a + b;
+        printf(", %d", temp);
+        a = b;
+        b = temp;
     }
 
-    for (int i = 1; i < n; i++) {
-        if (n % i == 0) {
-            somme += i;
-        }
-    }
-
-    if (somme == n)
-        printf("%d est un nombre parfait.\n", n);
-    else
-        printf("%d n'est pas un nombre parfait.\n", n);
-
+    printf("\n");
     return 0;
 }
