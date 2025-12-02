7f68b3cf43d08e9f01466e5a30a57528215aa2dd
Guillom <147560209+Gui-llom@users.noreply.github.com>
2025-12-02 11:20:22 +0100

    Update and rename couleurs.c to chercher2.c

---
+++ TP3/src/chercher2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    char phrases[10][100] = {
+        "Bonjour, comment ça va ?",
+        "Le temps est magnifique aujourd'hui.",
+        "C'est une belle journée.",
+        "La programmation en C est amusante.",
+        "Les tableaux en C sont puissants.",
+        "Les pointeurs en C peuvent être déroutants.",
+        "Il fait beau dehors.",
+        "La recherche dans un tableau est intéressante.",
+        "Les structures de données sont importantes.",
+        "Programmer en C, c'est génial."
+    };
+
+    char recherche[100];
+    int trouvé = 0;
+
+    printf("Entrez la phrase à rechercher :\n");
+    fgets(recherche, 100, stdin);
+
+
+    int len = 0;
+    while(recherche[len] != '\0') len++;
+    if(recherche[len-1] == '\n') recherche[len-1] = '\0';
+
+  for(int i = 0; i < 10; i++) {
+        int j = 0;
+        while(phrases[i][j] != '\0' && recherche[j] != '\0' && phrases[i][j] == recherche[j])
+            j++;
+
+        if(phrases[i][j] == '\0' && recherche[j] == '\0') {
+            trouvé = 1;
+            break;
+        }
+    }
+
+    if(trouvé)
+        printf("Phrase trouvée\n");
+    else
+        printf("Phrase non trouvée\n");
+
+    return 0;
+}
